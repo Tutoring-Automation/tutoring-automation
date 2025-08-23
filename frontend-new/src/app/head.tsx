306e95c7a0b10f2e471a5b0f94b34f628d714e24
adhamabouaita <adhamabouaita@mail.com>
2025-08-23 19:53:55 -0400

    Update favicon to ICO format in layout component and remove PNG version

---
+++ frontend-new/src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  );
+}
+
+
