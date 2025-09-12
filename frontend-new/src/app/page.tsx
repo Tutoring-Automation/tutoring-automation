9f87057196732c585d6571dd2dcada5463b5a6a1
adhamabouaita <adhamabouaita@mail.com>
2025-09-12 19:49:00 -0400

    skibidi dob dob dob yes yes

--- frontend-new/src/app/page.tsx
+++ frontend-new/src/app/page.tsx
@@ -1,57 +1,35 @@
 "use client";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    // Dynamically load the Tenor embed script
-    const script = document.createElement("script");
-    script.src = "https://tenor.com/embed.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    // Cleanup script on unmount
-    return () => {
-      if (document.body.contains(script)) {
-        document.body.removeChild(script);
-      }
-    };
-  }, []);
-
   return (
     <div 
       style={{ 
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
         width: "100vw", 
         height: "100vh",
         overflow: "hidden",
         backgroundColor: "#000",
         margin: 0,
-        padding: 0
+        padding: 0,
+        position: "relative"
       }}
     >
-      <div
-        className="tenor-gif-embed"
-        data-postid="15167809337439398898"
-        data-share-method="host"
-        data-aspect-ratio="1.77857"
-        data-width="100%"
-        style={{ 
-          width: "100vw", 
-          height: "100vh",
-          maxWidth: "100vw",
-          maxHeight: "100vh"
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/jjy0bIO-YNI?autoplay=1&mute=0&controls=1&showinfo=0&rel=0&modestbranding=1&playsinline=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          border: "none"
         }}
-      >
-        <a href="https://tenor.com/view/67-6-7-six-seven-sixty-seven-67-kid-gif-15167809337439398898">
-          67 6 7 GIF
-        </a>
-        from{" "}
-        <a href="https://tenor.com/search/67-gifs">
-          67 GIFs
-        </a>
-      </div>
+      ></iframe>
     </div>
   );
 }
